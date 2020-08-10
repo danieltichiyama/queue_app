819c6baf83090d620c9cafa3e54f102809f8e31f
Calvin <calmoon808@gmail.com>
2020-08-09 15:34:24 -1000

    twilio notification action added

--- src/actions/index.js
+++ src/actions/index.js
@@ -97,3 +97,15 @@ export const removeCustomerFromWaitlist = data => async dispatch => {
         console.log(err.message)
     })
 }
+
+export const notifyCustomer = () => async dispatch => {
+  await Axios.post("/api/sms/send").then(customer => {
+    console.log(customer);
+      dispatch({
+          type: TWILIO_NOTIFICATION,
+          payload: customer.data
+      })
+  }).catch(err => {
+      console.log(err.message)
+  })
+}
