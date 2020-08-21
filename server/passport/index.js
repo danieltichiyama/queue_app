5dbaa1da39b4d8221174f4080796f30e4be18de1
Calvin <calmoon808@gmail.com>
2020-08-20 17:56:38 -1000

    Local strategy implementex for login

---
+++ server/passport/index.js
@@ -0,0 +1,45 @@
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const { Retailer } = require("../models");
+
+const saltRounds = 12;
+
+passport.use(
+  "login",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password"
+    },
+    (username, password, done) => {
+      try {
+        Retailer.findOne({ username: username })
+        .then(async retailer => {
+          if (!retailer) {
+            return done(null, false, {message: "Username or password invalid." });
+          } else {
+            bcrypt.compare(password, retailer.password).then(result => {
+              if (!result) {
+                return done(null, false, { message: 'Username or password invalid. '});
+              }
+              console.log("user found & authenticated");
+              return done(null, retailer);
+            })
+          }
+        })
+      } catch (err) {
+        done(err)
+      }
+    }
+  )
+)
+
+passport.serializeUser(function(retailer, done) {
+  return done(null, { id: retailer._id, username: retailer.username, name: retailer.retailerName });
+});
+
+passport.deserializeUser(function(retailer, done) {
+  return done(null, retailer);
+});
