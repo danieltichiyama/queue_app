df32fd50ebcd955ab9b74640dc37e533d69af6c8
Kevinnnface <45556758+Kevinnnface@users.noreply.github.com>
2020-09-10 15:14:13 -1000

    need to verifiy sendgrid email

--- server/api/verification-routes.js
+++ server/api/verification-routes.js
@@ -4,7 +4,7 @@ const verificationRouter = require("express").Router();
 
 const { sendPIN, checkPIN } = require("../controllers/verificationController");
 
-verificationRouter.route("/send").post(sendPIN);
-verificationRouter.route("/check").post(checkPIN);
+verificationRouter.route("/:retailerId/send").post(sendPIN);
+verificationRouter.route("/:retailerId/check").post(checkPIN);
 
 module.exports = verificationRouter;
--- server/controllers/verificationController.js
+++ server/controllers/verificationController.js
@@ -11,21 +11,22 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const verificationController = {
   sendPIN({ params, body }, res) {
+    let verificationType = body.verificationType;
     let retailerId = params.retailerId;
     let randomPIN = Math.floor(100000 + Math.random() * 900000);
     let toNumber = body.number;
-    let toEmail = "kevinchguo@gmail.com";
-    let verificationType = body.verificationType;
+    let toEmail = body.email;
 
     Retailer.findByIdAndUpdate(
       { _id: retailerId },
       { verificationPIN: randomPIN }
     ).then((results) => {
+      let retailerName = results.retailerName;
       let verificationPIN = results.verificationPIN;
       if (verificationType === "text") {
         client.messages
           .create({
-            body: `Hello RETAILER_NAME, your verification PIN is ${verificationPIN}`,
+            body: `Hello ${retailerName}, your verification PIN is ${verificationPIN}`,
             from: twilioNumber,
             to: toNumber,
           })
@@ -34,18 +35,21 @@ const verificationController = {
       } else if (verificationType === "call") {
         client.calls
           .create({
-            twiml: `<Response><Say>Hello RETAILER_NAME, this is a call from Queued App. Your verification pin is ${verificationPIN}. Please verify your pin in our app. Again, your verification pin is ${verificationPIN}. Thank you.</Say></Response>`,
+            twiml: `<Response><Say>Hello ${retailerName}, this is a call from Queue App. Your verification pin is ${verificationPIN}. Please verify your pin in our app. Again, your verification pin is ${verificationPIN}. Thank you.</Say></Response>`,
           })
           .then((message) => console.log(message))
           .catch((err) => console.log(err));
       } else if (verificationType === "email") {
-        sgMail.send({
-          to: `${toEmail}`,
-          from: "kevinchguo@gmail.com",
-          subject: "Subject",
-          text: "Hello",
-          html: "<body>Hello</body>",
-        });
+        sgMail
+          .send({
+            to: `${toEmail}`,
+            from: "kevinchguo@gmail.com",
+            subject: "Queue App Verification",
+            text: `Hello ${retailerName}, your PIN is ${verificationPIN}. Please verify through our app. Thank you!`,
+            html: `<body><h1>Hello ${retailerName},</h1><p>your PIN is ${verificationPIN}. Please verify through our app. Thank you!</p></body>`,
+          })
+          .then((message) => console.log(message))
+          .catch((err) => console.log(err));
       }
     });
   },
