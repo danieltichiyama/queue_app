66b84f4273cd4f80998c32862cbe2cfef88cf28a
Kevinnnface <45556758+Kevinnnface@users.noreply.github.com>
2020-09-19 16:06:30 -1000

    added verification route into App

--- src/App/App.js
+++ src/App/App.js
@@ -3,10 +3,11 @@ import styles from "./App.module.scss";
 import { connect } from "react-redux";
 import RetailerView from "../views/RetailerView";
 import UserView from "../views/UserView";
-import RetailerProfileView from '../views/RetailerProfileView';
-import UserProfileView from '../views/UserProfileView';
-import AuthView from '../views/AuthView';
-import { Switch, Route } from 'react-router-dom';
+import RetailerProfileView from "../views/RetailerProfileView";
+import UserProfileView from "../views/UserProfileView";
+import AuthView from "../views/AuthView";
+import VerificationView from "../views/VerificationView";
+import { Switch, Route } from "react-router-dom";
 
 function App(props) {
   const [retailerLoggedIn, setRetailerLoggedIn] = useState(false);
@@ -24,23 +25,19 @@ function App(props) {
     <div className={styles.App}>
       <div className={styles.appMobileContainer}>
         <Switch>
+          <Route path="/userview" component={UserView} />
           <Route
-            path='/userview'
-            component={UserView} />
-          <Route
-            path='/retailerview'
-            component={retailerLoggedIn ? RetailerView : AuthView} />
-          <Route
-            path='/retailerprofile'
-            component={RetailerProfileView} />
-          <Route
-            path='/userprofile'
-            component={UserProfileView} />
+            path="/retailerview"
+            component={retailerLoggedIn ? RetailerView : AuthView}
+          />
+          <Route path="/retailerprofile" component={RetailerProfileView} />
+          <Route path="/verification" component={VerificationView} />
+          <Route path="/userprofile" component={UserProfileView} />
         </Switch>
       </div>
     </div>
   );
-};
+}
 const mapStateToProps = (state) => {
   return {
     retailers: state.retailers,
--- src/views/VerificationView/VerificationView.jsx
+++ src/views/VerificationView/VerificationView.jsx
@@ -2,14 +2,11 @@ import React, { Component } from "react";
 
 class VerificationView extends Component {
   constructor(props) {
+    super(props);
     this.state = {};
   }
   render() {
-    return (
-      <>
-        <div>VerificationView</div>
-      </>
-    );
+    return <h1>Verification View</h1>;
   }
 }
 
