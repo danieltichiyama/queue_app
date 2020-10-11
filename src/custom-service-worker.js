ea6d33d6a0f84f44675946416f11e64fe64b0f40
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2020-10-10 22:11:36 -1000

    added service worker logic, but something is still breaking...

--- src/custom-service-worker.js
+++ src/custom-service-worker.js
@@ -3,6 +3,60 @@
 // This file's functionality can only be tested on a build of the app:
 // npm run build && server -s build
 
-self.addEventListener("install", ()=>{
-    console.log('Hello');
-})
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+
+// Install the service worker
+self.addEventListener('install', function() {
+  console.log("Service worker installed...")
+});
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== "workbox-precache-v2-http://localhost:5000/" || key !== DATA_CACHE_NAME) {
+            //! added this thinking that it wouldn't delete the workbox-precache database but it's still breaking...
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Intercept fetch requests
+self.addEventListener('fetch', function(e) {
+
+ console.log("Service worker intercepting fetch request...")
+  if (e.request.url.includes('/api/')) {
+    e.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(e.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(e.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(e.request);
+            });
+        })
+        .catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+});
